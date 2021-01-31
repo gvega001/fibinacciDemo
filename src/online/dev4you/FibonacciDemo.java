5ac4ea2aafefd5ad868f5898dcd7310ab6d309c6
gvega001 <gvega001@gmail.com>
2021-01-31 10:58:14 -0800

    created TimeDate and implemented it with Duration and Instant using import java.time

--- src/online/dev4you/FibonacciDemo.java
+++ src/online/dev4you/FibonacciDemo.java
@@ -1,4 +1,16 @@
 package online.dev4you;
 
+import java.time.Duration;
+import java.time.Instant;
+
+class TimeDate {
+    public Instant start;
+    public Instant end;
+
+    //return total time in seconds
+    public double timeInSeconds() {
+        return Duration.between(start, end).toMillis() / 1000.0;
+    }
+}
 public class FibonacciDemo {
 }
